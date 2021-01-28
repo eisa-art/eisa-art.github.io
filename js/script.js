6d79c2e051ce5a70272df53796837b5e31ffe93a
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2021-01-28 09:47:52 +0330

    fix: move script at the end of body

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  const nav = document.querySelector("#main-nav");
+const nav = document.querySelector("#main-nav");
 
-  const removeActiveNav = () => {
-    const navItems = document.querySelectorAll("#main-nav ul li a");
-    for (let i = 0; i < navItems.length; i++) {
-      navItems[i].classList.remove("active");
+const removeActiveNav = () => {
+  const navItems = document.querySelectorAll("#main-nav ul li a");
+  for (let i = 0; i < navItems.length; i++) {
+    navItems[i].classList.remove("active");
+  }
+};
+
+nav.addEventListener(
+  "click",
+  function (event) {
+    if (event.target.tagName.toLowerCase() === "a") {
+      removeActiveNav();
+      const link = event.target;
+      link.classList.add("active");
     }
-  };
-
-  nav.addEventListener(
-    "click",
-    function (event) {
-      if (event.target.tagName.toLowerCase() === "a") {
-        removeActiveNav();
-        const link = event.target;
-        link.classList.add("active");
-      }
-    },
-    false
-  );
-});
+  },
+  false
+);
