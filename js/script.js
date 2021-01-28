a356d82d3104c461d40a46025263348a6de7f681
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2021-01-28 09:56:22 +0330

    refactor: add a function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const nav = document.querySelector("#main-nav");
 
+// remove styles from navigation a tags
 const removeActiveNav = () => {
   const navItems = document.querySelectorAll("#main-nav ul li a");
   for (let i = 0; i < navItems.length; i++) {
@@ -7,14 +8,12 @@ const removeActiveNav = () => {
   }
 };
 
-nav.addEventListener(
-  "click",
-  function (event) {
-    if (event.target.tagName.toLowerCase() === "a") {
-      removeActiveNav();
-      const link = event.target;
-      link.classList.add("active");
-    }
-  },
-  false
-);
+const addActiveClass = (event) => {
+  if (event.target.tagName.toLowerCase() === "a") {
+    removeActiveNav();
+    const link = event.target;
+    link.classList.add("active");
+  }
+};
+
+nav.addEventListener("click", addActiveClass);
