6c5765eeb5c0f91ed28c6d3f6c8fcb26407b40fa
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2021-01-28 09:36:50 +0330

    feat: added js to add class to nav menu

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  const nav = document.querySelector("#main-nav");
+
+  const removeActiveNav = () => {
+    const navItems = document.querySelectorAll("#main-nav ul li a");
+    for (let i = 0; i < navItems.length; i++) {
+      navItems[i].classList.remove("active");
+    }
+  };
+
+  nav.addEventListener(
+    "click",
+    function (event) {
+      if (event.target.tagName.toLowerCase() === "a") {
+        removeActiveNav();
+        const link = event.target;
+        link.classList.add("active");
+      }
+    },
+    false
+  );
+});
